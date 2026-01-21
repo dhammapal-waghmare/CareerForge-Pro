24c18069b6def87228672e837b00f01ff07efda1
dhammapal-waghmare <dhammapalwaghmare2020@gmail.com>
2026-01-21 12:45:58 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+function App() {
+  return (
+    <>
+      <h1>Welcome to CareerForge-Pro</h1>
+      <h3>This is my professional career development platform.</h3>
+    </>
+  );
+}
+
+export default App;
